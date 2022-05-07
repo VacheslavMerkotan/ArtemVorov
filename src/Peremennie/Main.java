ef42dd86927334b4259074a6f0afd17f1a36e9a5
VacheslavMerkotan <Nihil_13@mail.ru>
2022-05-07 11:45:18 +0300

    Введение понятия ссылочного типа память

--- src/Peremennie/Main.java
+++ src/Peremennie/Main.java
@@ -3,23 +3,19 @@ package Peremennie;
 public class Main {
 
     public static void main(String[] args) {
-        byte b = 127;
-        short s = 32000;
-        char c = 98;
-        int i = 355555678;
-        long l = 3547899003567L;
-        boolean bool = false;
-        float f = 23.5F;
-        double d = 23.67788876554;
+        byte b = 127; //Byte
+        short s = 32000;//Short
+        char c = 98;//Character
+        int i = 355555678;//Integer
+        long l = 3547899003567L;//Long
+        boolean bool = false;//Boolean
+        float f = 23.5F;//Float
+        double d = 23.67788876554;//Double
+        String str = "25";
+        int num = Integer.parseInt(str);
+        System.out.println(num * 2);
 
-        System.out.println(d);
 
-        i = 100;
-        System.out.println(i);
-
-        int num = 123;
-        i = num;
-        System.out.println(i);
 
 
     }
